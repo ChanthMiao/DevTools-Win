ee133cc6696b4c1c65954564141eb9a6c7b2dde5
ChanthMiao <chanthmiao@outlook.com>
2021-09-27 12:28:43 +0800

    Fix max length limit bug of New-RandString.

--- src/public/core/New-RandString.ps1
+++ src/public/core/New-RandString.ps1
@@ -1,6 +1,20 @@
+function random_ascii_nums {
+    param (
+        [Parameter()]
+        [Int32]
+        $Times
+    )
+
+    $raws = (0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A)
+    for ($i = 0; $i -lt $Times; $i++) {
+        Get-Random -Count 62 -InputObject $raws | Write-Output
+    }
+}
+
 function New-RandString {
     param (
         [Parameter()]
+        [ValidateScript({ $_ -gt 0 })]
         [Int32]
         $Length
     )
@@ -8,7 +22,11 @@ function New-RandString {
         if (-not $Length) {
             $Length = 8
         }
-        [string]::Concat(((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count $Length).forEach( { [char]$_ }))
+        $mul = 1;
+        if ($Length -gt 62) {
+            $mul = [int]($Length / 62) + 1
+        }
+        -join (random_ascii_nums -Times $mul | Get-Random -Count $Length | ForEach-Object { [char]$_ })
     }
 }
 
